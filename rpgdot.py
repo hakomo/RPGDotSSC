5b4131b90cc9b647ddcca2d79997b30d6edba685
hakomo <hakomoa@gmail.com>
2014-07-08 21:10:35 +0900

    change aspect ratio to 2

--- rpgdot.py
+++ rpgdot.py
@@ -1,5 +1,8 @@
+from bisect import *
 from glob import *
+from itertools import *
 import json
+from math import *
 import os
 import re
 import shutil
@@ -7,11 +10,71 @@ from zipfile import *
 from PIL import Image
 import wget
 
-def image(fn):
+class Accumulate:
+
+    def __init__(self, a):
+        self.a = [0] + list(accumulate(a))
+
+    def end(self, bg, sm):
+        return bisect_right(self.a, self.a[bg] + sm, bg + 1) - 1
+
+    def sum(self, bg, ed):
+        return self.a[ed] - self.a[bg]
+
+    def __len__(self):
+        return len(self.a) - 1
+
+class SegmentTree:
+
+    def __init__(self, a):
+        ln = 2 ** ceil(log2(len(a)))
+        self.a = [0] * (ln - 1) + a + [-1 << 30] * (ln - len(a))
+        for i in range(ln - 2, -1, -1):
+            self.a[i] = max(self.a[i * 2 + 1], self.a[i * 2 + 2])
+
+    def max(self, bg, ed):
+        return self.rmax(bg, ed, 0, 0, len(self.a) // 2 + 1)
+
+    def rmax(self, bg, ed, ix, l, r):
+        if r <= bg or ed <= l:
+            return -1 << 30
+        if bg <= l and r <= ed:
+            return self.a[ix]
+        return max(self.rmax(bg, ed, ix * 2 + 1, l, (l + r) // 2),
+            self.rmax(bg, ed, ix * 2 + 2, (l + r) // 2, r))
+
+def transparentImage(fn):
     f = Image.open(fn)
-    img = Image.new('RGB', f.size)
+    img = Image.new('RGBA', f.size)
     img.paste(f, (0, 0))
+    p = img.load()
+    c = p[0, 0] if p[0, 0] == p[5, 0] else p[9, 0]
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if p[x, y] == c:
+                p[x, y] = (0, 0, 0, 0)
     return img
+
+def height(ac, st, w):
+    h, bg = 0, 0
+    while bg < len(ac):
+        ed = ac.end(bg, w)
+        h += st.max(bg, ed)
+        bg = ed
+    return h
+
+def diff(ac, st, w):
+    return abs(w / height(ac, st, w) - 2)
+
+def size(ac, st, w):
+    l, r = w, ac.sum(0, -1)
+    while r - l > 2:
+        if diff(ac, st, l + (r - l) // 3) < diff(ac, st, l // 3 + r * 2 // 3):
+            r = l // 3 + r * 2 // 3
+        else:
+            l += (r - l) // 3
+    return min(((w, height(ac, st, w)) for w in range(l, r + 1)),
+        key=lambda s: abs(s[0] / s[1] - 2))
 
 for i in range(1, 27):
     zn = wget.download(
@@ -27,45 +90,28 @@ for i in range(1, 27):
             shutil.copyfile(fp, 'm{:0>3}.bmp'.format(int(fp[-7:-4]) * 2))
     shutil.rmtree(zn[:-4])
 
-smw, mxh, mxn = 0, 0, 0
-bcs, fcs = set(), set()
-for fn in iglob('m*.bmp'):
-    img = image(fn)
+imgs = [(int(fn[1:4]), transparentImage(fn)) for fn in iglob('m*.bmp')]
+imgs.sort(key=lambda v: v[1].size[1])
 
-    smw += img.size[0]
-    mxh = max(mxh, img.size[1])
-    mxn = max(mxn, int(fn[1:4]))
+ws, hs, mxw, mxn = [], [], 0, 0
+for ix, img in imgs:
+    ws.append(img.size[0])
+    hs.append(img.size[1])
+    mxw = max(mxw, img.size[0])
+    mxn = max(mxn, ix + 1)
 
-    bc = img.getpixel((0, 0))
-    bcs.add(bc)
-    fcs |= {c[1] for c in img.getcolors() if c[1] != bc}
-
-for c in bcs:
-    if c in fcs: continue
-    bc = c
-    break
-else:
-    print('not fount back color')
-    exit()
-
-a = [None] * (mxn + 1)
-dst = Image.new('RGBA', (smw, mxh))
-l = 0
-for fn in iglob('m*.bmp'):
-    img = image(fn).convert('RGBA')
-    p = img.load()
-    c = p[0, 0]
-    for y in range(img.size[1]):
-        for x in range(img.size[0]):
-            if p[x, y] == c:
-                p[x, y] = (0, 0, 0, 0)
-    dst.paste(img, (l, 0))
-
-    a[int(fn[1:4])] = l, img.size[0], img.size[1]
-
-    l += img.size[0]
+bs = [None] * mxn
+dstimg = Image.new('RGBA', size(Accumulate(ws), SegmentTree(hs), mxw))
+x = y = h = 0
+for ix, img in imgs:
+    if x + img.size[0] > dstimg.size[0]:
+        y += h
+        x = h = 0
+    bs[ix] = x, y, img.size[0], img.size[1]
+    dstimg.paste(img, (x, y))
+    x += img.size[0]
+    h = max(h, img.size[1])
 
 with open('rpgdot.json', 'w') as f:
-    json.dump(a, f)
-
-dst.save('rpgdot.png', 'png')
+    json.dump(bs, f)
+dstimg.save('rpgdot.png', 'png')
